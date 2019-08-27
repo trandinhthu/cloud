8e28db35b782658787698936c278e5ce3ea453b2
trandinhthu <52957439+trandinhthu@users.noreply.github.com>
2019-08-27 14:53:12 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <?php 
     session_start();
     include_once "connect.php";
-    echo "<h1>Sss</h1>"; 
     $sql = "SELECT * FROM account where name = :usernamestore and mk = :pass";
     $stmt = $pdo->prepare($sql);
     $stmt->execute
@@ -22,28 +21,5 @@
     else
     {
         echo "<h1>Sai username or pass</h1>";
-    }
-
-    $sql = "SELECT * FROM manage where name = :username and mk = :pass";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute
-    (
-        array(
-            'username' => $_POST['username'],
-            'pass' => $_POST['pass'],
-        )
-    );
-    $count = $stmt->rowCount();
-    if($count > 0)
-    {   
-        $_SESSION['username'] = $_POST['username'];
-        header("Location: capcao.php");
-        
-    }
-    else
-    {
-        echo "<h1>Sai username or pass</h1>";
-    }
-
-        
+    }      
 ?>
