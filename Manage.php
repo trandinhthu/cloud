18b871cd87099b85c8affb551b6f6f7b87358a58
TDT <52957439+trandinhthu@users.noreply.github.com>
2019-08-27 13:28:29 +0700

    Update Manage.php

--- Manage.php
+++ Manage.php
@@ -1,10 +1,10 @@
-<?php 
+<!-- <?php 
     session_start();
     if(!isset($_SESSION['username']))
     {
         header("Location: index.php");
     }
-?> 
+?>  -->
 <!DOCTYPE html>
 <html lang="en">
 
